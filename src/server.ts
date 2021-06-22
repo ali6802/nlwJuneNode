ba8354c88b76a45a871d4585c20a09245700ac2b
ali6802 <ali6802.dev@gmail.com>
2021-06-21 22:30:22 -0300

    sqlite funcionando

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
+import "reflect-metadata";
+
 import express from "express";
 
+import "./database";
+
 const app= express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.get("/test",(req,res)=>{
-   res.json({message:'Hello NLW'})
-})
-//primeira rota post
-app.post("/test/post",(req,res)=>{
-   res.json({message:"Hello NLW, Primeira Rota Post"})
-})
 
-app.listen(port,()=>console.log('Server is listening on port',port))
+app.listen(port,()=>console.log('Server is listening on port',port));
 
